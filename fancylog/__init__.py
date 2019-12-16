1a1fa784a06c282b90a05777d133c79b3e98109b
Adam Tyson <adam.tyson@ucl.ac.uk>
2019-12-16 18:03:14 +0000

    check for updates

---
+++ fancylog/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.0.3"
+__author__ = "Adam Tyson"
+__license__ = "GPL-3.0"
+__name__ = "fancylog"
+
+from . import *
+
+import luddite
+from packaging import version
+
+most_recent_version = luddite.get_version_pypi(__name__)
+
+if version.parse(__version__) < version.parse(most_recent_version):
+    print(f"This version of {__name__} ({__version__}) is not the most recent "
+          f"version. Please update to v{most_recent_version} by running: "
+          f"'pip install {__name__} --upgrade'")
