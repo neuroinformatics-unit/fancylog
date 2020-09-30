3bb87bac0782ea92b85f9b3b3de4f715b5882285
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-09-30 10:03:49 +0100

    Bump version: 0.0.10 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fancylog",
-    version="0.0.10",
+    version="0.1.0",
     description="Fancier logging in Python",
     install_requires=["packaging"],
     extras_require={
